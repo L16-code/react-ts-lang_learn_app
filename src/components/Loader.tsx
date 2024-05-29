8b649962dfa9d90e983bdf40e0aa7f2e50c34507
L16-code <lavishjain2002@gmail.com>
2024-05-29 15:56:24 +0530

    solved loader problem once again

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,22 @@
-import { Container } from '@mui/material'
-import gif from '../assets/Spinner@1x-1.0s-200px-200px.gif'
-import '../css/Loader.css'
-// import { Image } from '@mui/icons-material'
-const Loader = () => {
-    return <Container className="loader-container">
-        <img src={gif} alt="loading" />
-    </Container>
-}
 
-export default Loader
+import gif from '../assets/Spinner@1x-1.0s-200px-200px.gif';
+import { Container, Box } from '@mui/material';
+
+const Loader = () => {
+    return (
+        <Container maxWidth="sm">
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh', // Full viewport height
+                }}
+            >
+                <img src={gif} alt="loading" />
+            </Box>
+        </Container>
+    );
+};
+
+export default Loader;
