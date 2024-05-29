81837c80c1c4c66f9dabfa26615ec3a5af252165
L16-code <lavishjain2002@gmail.com>
2024-05-29 15:41:13 +0530

    added the loader in the page

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,11 @@
-
+import { Container } from '@mui/material'
+import gif from '../assets/Spinner@1x-1.0s-200px-200px.gif'
+import '../css/Loader.css'
+// import { Image } from '@mui/icons-material'
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
+    return <Container maxWidth="sm" className="loader-container">
+        <img src={gif} alt="loading" />
+    </Container>
 }
 
 export default Loader
